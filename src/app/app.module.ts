78edd541e58481694d9d61f7bd91c4de2740114a
ejangphoebe <ejangphoebe65@gmail.com>
2021-08-08 10:38:51 +0300

    ui corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,19 +13,19 @@ import { LoginComponent } from './Auth/login/login.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { AddUsersComponent } from './Pages/add-users/add-users.component';
 import { TokenInterceptorService } from './Interceptor/token-interceptor.service';
-import { AddwebSecurityComponent } from './Pages/addweb-security/addweb-security.component';
-import { WebSecurityLevelsComponent } from './Pages/web-security-levels/web-security-levels.component';
+import { AddwebSecurityComponent } from './Pages/Web-security/addweb-security/addweb-security.component';
+import { WebSecurityLevelsComponent } from './Pages/Web-security/web-security-levels/web-security-levels.component';
 import { UserLogsComponent } from './user-logs/user-logs.component';
 import { UsersListComponent } from './Pages/users-list/users-list.component';
 // import { CdComponent } from './Pages/cd/cd.component';
-import { AddWellboreComponent } from './Pages/add-wellbore/add-wellbore.component';
-import { AddFileFormatsComponent } from './Pages/add-file-formats/add-file-formats.component';
-import { AddFileSecurityGradeComponent } from './Pages/add-file-security-grade/add-file-security-grade.component';
-import { WellboreCoreComponent } from './Pages/wellbore-core/wellbore-core.component';
-import { AddCoreCatalogComponent } from './Pages/add-core-catalog/add-core-catalog.component';
-import { WellboresComponent } from './Pages/wellbores/wellbores.component';
-import { FileFormatsComponent } from './Pages/file-formats/file-formats.component';
-import { FileSecurityGradesComponent } from './Pages/file-security-grades/file-security-grades.component';
+import { AddWellboreComponent } from './Pages/Wellbores-/add-wellbore/add-wellbore.component';
+import { AddFileFormatsComponent } from './Pages/File-format/add-file-formats/add-file-formats.component';
+import { AddFileSecurityGradeComponent } from './Pages/File-security/add-file-security-grade/add-file-security-grade.component';
+import { WellboreCoreComponent } from './Pages/Wellbore-core-/wellbore-core/wellbore-core.component';
+import { AddCoreCatalogComponent } from './Pages/Core-catalog/add-core-catalog/add-core-catalog.component';
+import { WellboresComponent } from './Pages/Wellbores-/wellbores/wellbores.component';
+import { FileFormatsComponent } from './Pages/File-format/file-formats/file-formats.component';
+import { FileSecurityGradesComponent } from './Pages/File-security/file-security-grades/file-security-grades.component';
 import { BaseComponent } from './base/base.component';
 import { CoreTypesComponent } from './Pages/CoreTypes/core-types/core-types.component';
 import { ViewCoreTypeComponent } from './Pages/CoreTypes/view-core-type/view-core-type.component';
@@ -35,8 +35,8 @@ import { CatalogSecurityComponent } from './Pages/CatalogSecurity/catalog-securi
 import { ViewCatalogueSecurityLevelsComponent } from './Pages/CatalogSecurity/view-catalogue-security-levels/view-catalogue-security-levels.component';
 import { StratLithoUnitComponent } from './Pages/StratLithoUnit/strat-litho-unit/strat-litho-unit.component';
 import { ViewStratLithoUnitComponent } from './Pages/StratLithoUnit/view-strat-litho-unit/view-strat-litho-unit.component';
-import { WellsComponent } from './Pages/wells/wells.component';
-import { CoreCatalogsComponent } from './Pages/core-catalogs/core-catalogs.component';
+import { WellsComponent } from './Pages/Wellbore-core-/wells/wells.component';
+import { CoreCatalogsComponent } from './Pages/Core-catalog/core-catalogs/core-catalogs.component';
 
 @NgModule({
   declarations: [
