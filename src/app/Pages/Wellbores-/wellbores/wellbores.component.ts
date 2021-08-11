1d96b81f83d09fd16d552e7afad00f5d3bd7251b
Freedom Gemmar <freedomgemmar1@gmail.com>
2021-08-11 13:49:04 +0300

    well update

--- src/app/Pages/Wellbores-/wellbores/wellbores.component.ts
+++ src/app/Pages/Wellbores-/wellbores/wellbores.component.ts
@@ -4,6 +4,8 @@ import { ApiPipeService } from 'src/app/Services/api-pipe.service';
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { FormGroup } from '@angular/forms';
+import { NgPopupsService } from 'ng-popups';
 
 
 @Component({
@@ -12,68 +14,80 @@ import { Subject } from 'rxjs';
   styleUrls: ['./wellbores.component.css']
 })
 export class WellboresComponent implements OnInit {
+  formGroup!: FormGroup;
+  title!: string;
+  prospectIds: any;
+  licenceIds: any
   // dtOptions: DataTables.Settings = {};
   dtOptions: any = {};
   dtTrigger: Subject<any> = new Subject<any>();
+  allusers: any;
+  id: any;
+  posts: any = [];
+  deleteresp: any;
+  status: boolean = true;
+  editform: boolean = false;
+  updatevalue: any;
 
-wellbores: any = [];
-role:any;
-userEmail:any;
-loggedin:any;
+  wellbores: any = [];
+  role: any;
+  userEmail: any;
+  loggedin: any;
 
-constructor(
-  private authservice: ApiPipeService,
-  private router: Router,
-  private toastr: ToastrService,
-  private http: HttpClient
-) { }
+  constructor(
+    private authservice: ApiPipeService,
+    private router: Router,
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private ngPopups: NgPopupsService
+  ) { }
 
-ngOnInit(): void {
-  // this.authservice.reload();
-  this.userList()
-  this.userEmail = this.authservice.getEmail();
-  this.loggedin = this.authservice.getRole();
-  if(this.authservice.getRole()=="Admin"){
-    this.role=true;
-  }else{
-  this.role= false;
+  ngOnInit(): void {
+    // this.authservice.reload();
+    this.userList()
+    this.userEmail = this.authservice.getEmail();
+    this.loggedin = this.authservice.getRole();
+    if (this.authservice.getRole() == "Admin") {
+      this.role = true;
+    } else {
+      this.role = false;
+    }
+    this.dtOptions = {
+      dom: 'Bfrtip',
+      // dom:'Btp',
+      buttons: [
+        // 'columnsToggle',
+        // 'colvis',
+        {
+          extend: 'copy',
+          tag: 'button',
+          className: "btn blue btn-outline"
+        },
+        {
+          extend: 'print',
+          tag: 'button',
+          className: "btn yellow btn-outline"
+        },
+        {
+          extend: 'excel',
+          tag: 'button',
+          className: "btn green btn-outline"
+        },
+        {
+          extend: 'pdf',
+          tag: 'button',
+          className: "btn red btn-outline"
+        },
+      ]
+    }
   }
-  this.dtOptions = {
-    dom:'Bfrtip',
-    // dom:'Btp',
-    buttons: [
-      // 'columnsToggle',
-      // 'colvis',
-      {
-        extend:'copy',
-        tag: 'button',
-        className: "btn blue btn-outline"
-      },
-      {
-        extend:'print',
-        tag: 'button',
-        className: "btn yellow btn-outline"
-      },
-      {
-        extend:'excel',
-        tag: 'button',
-        className: "btn green btn-outline"
-      },
-      {
-        extend:'pdf',
-        tag: 'button',
-        className: "btn red btn-outline"
-      },
-    ]
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
-}
 
-ngOnDestroy(): void {
-  this.dtTrigger.unsubscribe();
-}
-
-userList(): void {
-  this.authservice
+  userList(): void {
+    this.authservice
       .getWellbores()
       .subscribe((response: any) => {
         console.log(response)
@@ -81,10 +95,144 @@ userList(): void {
 
         this.dtTrigger.next();
       });
-    }
+  }
 
-logout(){
-  this.authservice.logoutuser()
-}
+  onSelect(selectedItem: any) {
+    this.id = selectedItem.WebSecurityLevel_id
+
+    this.ngPopups.confirm("Are you sure you want to delete ?", {
+      // theme: 'material',
+      color: 'OrangeRed',
+      okButtonText: 'Yes',
+      cancelButtonText: 'No',
+      title: "Confirm",
+    })
+      .subscribe(res => {
+        if (res) {
+          console.log("Selected item Id: ", selectedItem.WebSecurityLevel_id);
+          this.http.delete('http://127.0.0.1:8899/apiv1/delete_web_security_level/' + this.id)
+            .subscribe(response => {
+              this.deleteresp = response;
+              console.log(this.deleteresp.message)
+              if (this.deleteresp.message == "Web security level successfully deleted.") {
+                this.toastr.success("Web security level successfully deleted.", "", {
+                  timeOut: 2000,
+                  positionClass: 'toast-top-center',
+                  progressBar: true,
+                  progressAnimation: 'increasing'
+                })
+                setTimeout(() => {
+                  this.authservice.reload();
+                }, 1000);
+
+              } else {
+                this.authservice.securityStatusUpdate()
+              }
+              console.log(this.deleteresp)
+            });
+        } else {
+          console.log("You clicked cancel.")
+        }
+      });
+
+  }
+
+  onSelectEdit(selectedItem: any) {
+    console.log("hide the elements");
+    this.status = false;
+    this.editform = true;
+    this.id = selectedItem.WebSecurityLevel_id
+    localStorage.setItem("update-id", this.id);
+    console.log("Selected item Id: ", selectedItem.WebSecurityLevel_id);
+    this.http.get('http://127.0.0.1:8899/apiv1/get_web_security_level/' + this.id)
+      .subscribe(response => {
+        this.updatevalue = response;
+        this.formGroup.patchValue({
+
+          WebSecurityLevelAbbreviation: this.updatevalue.WebSecurityLevelAbbreviation,
+          WebSecurityLevelName: this.updatevalue.WebSecurityLevelName,
+          WebSecurityLevelDescription: this.updatevalue.WebSecurityLevelDescription,
+          Comments: this.updatevalue.Comments
+        });
+        console.log(this.updatevalue)
+      });
+
+
+  }
+
+  navigateBack() {
+    // this.router.navigate(['/web-security-levels']);
+    this.authservice.reload();
+
+  }
+
+  getLicenseIds() {
+    this.authservice.getAllCompanies().subscribe(res => {
+      this.licenceIds = res;
+      console.log(this.licenceIds);
+    })
+  }
+
+  getProspectIds() {
+    this.authservice.getAllCompanies().subscribe(res => {
+      this.prospectIds = res;
+      console.log(this.prospectIds);
+    })
+  }
+
+  changeProspect(e: any) {
+    console.log(e.value)
+    this.prospectIds.setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+  changeLicense(e: any) {
+    console.log(e.value)
+    this.licenceIds.setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+
+  updateWellboreProcess(){
+    console.log("tested")
+    if(this.formGroup.valid){
+      console.log(this.formGroup.value)
+      this.authservice.adddWellbore(this.formGroup.value).subscribe(result =>{
+       
+        if(result.message == "Wellbore added successfuly."){
+          this.toastr.success("Wellbore added successfuly.","",{
+            timeOut: 2000,
+            positionClass: 'toast-top-center',
+            progressBar: true,
+            progressAnimation:'increasing'
+          })
+          this.formGroup.reset();
+          
+        } else{          
+          this.authservice.wellboreFaliure()
+        }
+      }, error => {
+        
+        console.log('oops', error.message)
+        if(error){
+          this.toastr.error(error.error.message,"",{
+            timeOut: 2000,
+            positionClass: 'toast-top-center',
+            progressBar: true,
+            progressAnimation:'decreasing'
+          })
+          // this.authservice.CompanyFaliure()
+        }
+      }
+      
+      )
+    }
+  }
+
+  logout() {
+    this.authservice.logoutuser()
+  }
 
 }
