9d4b1c7e0b7dc7d6fe88836bdd8ba966b80f6454
gemmar <gemmar@Freedomss-MacBook-Pro.local>
2022-04-06 11:57:51 +0300

    add companyType

--- src/app/Pages/Company/view-companies/view-companies.component.ts
+++ src/app/Pages/Company/view-companies/view-companies.component.ts
@@ -45,10 +45,8 @@ export class ViewCompaniesComponent implements OnInit, OnDestroy{
   checkstaff:any;
   maxd: any;
   mindate: any;
-
+  companyType: any = ['Operator', 'Licensing']
   Countries:any = ['Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', '$_[', 'Croatia', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russia', 'Rwanda', 'Saint Barthélemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'São Tomé and Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe']
-
-
 
   constructor(
     private authservice: ApiPipeService,
@@ -188,7 +186,7 @@ export class ViewCompaniesComponent implements OnInit, OnDestroy{
       NSDNumber: selectedItem.NSDNumber,
       CompanyCategoryId: selectedItem.CompanyCategoryId,
       Country: selectedItem.Country,
-      // CountryOfRegistrationId: selectedItem.CountryOfRegistrationId,
+      CompanyType: selectedItem.CompanyType.replace("CompanyTypeEnum.", ''),
       RegistrationNumber: selectedItem.RegistrationNumber,
       TINNumber: selectedItem.TINNumber,
       CompanyTelephone: selectedItem.CompanyTelephone,
@@ -270,7 +268,7 @@ export class ViewCompaniesComponent implements OnInit, OnDestroy{
            NSDNumber: this.authservice.stripFormValue(this.updatevalue.NSDNumber),
            CompanyCategoryId: this.authservice.stripFormValue(this.updatevalue.CompanyCategoryId),
            Country: this.authservice.stripFormValue(this.updatevalue.Country),
-          // CountryOfRegistrationId: this.authservice.stripFormValue(this.updatevalue.CountryOfRegistrationId),
+           CompanyType: this.authservice.stripFormValue(this.updatevalue.CompanyType.replace("CompanyTypeEnum.", '')),
            RegistrationNumber: this.authservice.stripFormValue(this.updatevalue.RegistrationNumber),
            TINNumber: this.authservice.stripFormValue(this.updatevalue.TINNumber),
            CompanyTelephone: this.authservice.stripFormValue(this.updatevalue.CompanyTelephone),
@@ -374,6 +372,7 @@ export class ViewCompaniesComponent implements OnInit, OnDestroy{
       NSDNumber:new FormControl(),
       CompanyCategoryId:new FormControl(),
       Country:new FormControl(),   
+      CompanyType:new FormControl(),
       RegistrationNumber:new FormControl(),
       TINNumber:new FormControl(),
       CompanyTelephone:new FormControl(),
@@ -426,6 +425,13 @@ export class ViewCompaniesComponent implements OnInit, OnDestroy{
   changeCountries(e:any) {
     console.log(e.value)
     this.Countries.setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+  changeCompanyType(e: any) {
+    console.log(e.value)
+    this.companyType.setValue(e.target.value, {
       onlySelf: true
     })
   }
